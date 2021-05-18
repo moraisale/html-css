3ea0b1c1bd65b5f8f986041730f07be8e2e0d999
Alexandre Morais <alexandremorais26@gmail.com>
2021-05-18 09:30:08 -0300

    Atualizando as telas

---
+++ script.js
@@ -0,0 +1,6 @@
+function enviar() {
+  let email = document.getElementById('username').value
+  window.alert(`Foi enviado um email para ${email} com as instruções de recuperação de senha`)
+  window.close()
+}
+
